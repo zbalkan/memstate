2035b3467607349366efc2cc56f00613cbf4188c
Zafer Balkan <zafer@zaferbalkan.com>
2026-01-22 14:51:44 +0200

    Used MessagePack for binary serialization

--- src/Memstate.Core/BinaryFormatterAdapter.cs
+++ src/Memstate.Core/BinaryFormatterAdapter.cs
@@ -1,16 +1,19 @@
+using MessagePack;
 using System.IO;
-using System.Runtime.Serialization.Formatters.Binary;
 
 namespace Memstate
 {
 	public class BinaryFormatterAdapter : BinarySerializer
 	{
-		readonly BinaryFormatter _formatter = new BinaryFormatter();
 
-		public override object ReadObject(Stream stream)
-			=> _formatter.Deserialize(stream);
+        public BinaryFormatterAdapter()
+        {
+            MessagePackSerializer.DefaultOptions = MessagePack.Resolvers.ContractlessStandardResolver.Options;
+        }
+        public override object ReadObject(Stream stream)
+			=> MessagePackSerializer.Deserialize<object>(stream);
 
 		public override void WriteObject(Stream stream, object @object)
-			=> _formatter.Serialize(stream, @object);
+			=> MessagePackSerializer.Serialize(stream, @object);
 	}
 }
